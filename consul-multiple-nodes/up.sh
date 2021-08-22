75b35dae3bfe74582f9ed3d0faef93401797f83e
JONAS Cheng <jonas.cheng@gmail.com>
2021-08-22 11:29:24 +0800

    up script support osx

--- consul-multiple-nodes/up.sh
+++ consul-multiple-nodes/up.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-HOSTIP=`hostname --ip-address` docker-compose --env-file ./config/server1.env up
+
+OS="$(uname -s)"
+
+case "${OS}" in
+    Darwin*)    HOSTIP=`ipconfig getifaddr en0` docker-compose --env-file ./config/server1.env up;;
+    *)          HOSTIP=`hostname --ip-address` docker-compose --env-file ./config/server1.env up;;
+esac
 
