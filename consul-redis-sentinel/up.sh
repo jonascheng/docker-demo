de8d807727f2aaefeb113a5959d894f08ffe79c2
Jonas Cheng <jonas_cheng@trendmicro.com>
2021-10-21 20:11:20 +0800

    feat(redis): master/slave with 2 nodes

--- consul-redis-sentinel/up.sh
+++ consul-redis-sentinel/up.sh
@@ -5,6 +5,11 @@ OS="$(uname -s)"
 docker rm -f `docker ps -qa`
 
 case "${OS}" in
-    Darwin*)    HOSTIP=`ipconfig getifaddr en0` docker-compose up;;
-    *)          HOSTIP=`hostname --ip-address` docker-compose up;;
+    Darwin*)    HOSTIP=`ipconfig getifaddr en0`;;
+    *)          HOSTIP=`hostname --ip-address`;;
+esac
+
+case "${HOSTIP}" in
+    10.1.0.10*) HOSTIP=${HOSTIP} REPLICATION=master docker-compose up;;
+    *)          HOSTIP=${HOSTIP} REPLICATION=slave  docker-compose up;;
 esac
