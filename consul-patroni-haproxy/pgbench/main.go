b54f1d83832fec5a813445858a5346389381974a
Jonas Cheng <jonas_cheng@trendmicro.com>
2021-11-17 13:21:16 +0800

    feat(bench): log to file

--- consul-patroni-haproxy/pgbench/main.go
+++ consul-patroni-haproxy/pgbench/main.go
@@ -253,16 +253,16 @@ func StartBench(ctx context.Context) {
 
 func main() {
 	// log to custom file
-	LOG_FILE := fmt.Sprintf("/tmp/pgbench-%d.log", time.Now().Unix())
+	logFilename := fmt.Sprintf("/tmp/pgbench-%d.log", time.Now().Unix())
 	// open log file
-	logFile, err := os.OpenFile(LOG_FILE, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
+	logFile, err := os.OpenFile(logFilename, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatalf("error: %v\n", err)
 	}
 	defer logFile.Close()
 
-	// // Set log out put and enjoy :)
-	// log.SetOutput(logFile)
+	// Set log out put
+	log.SetOutput(logFile)
 
 	// optional: log date-time, filename, and line number
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
