3ce3647296de5b72edc9dc770de7b11c2d3b40a0
Jonas Cheng <jonas_cheng@trendmicro.com>
2021-12-23 20:02:09 +0800

    feat(bench): init bench topic

---
+++ consul-kafka-zookeeper/bench/create_bench.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+DOCKER_IMAGE=docker.io/bitnami/kafka:2.5.0-debian-10-r112
+KAFKA_HOST=10.1.0.10
+KAFKA_PORT=19092
+KAFKA_BENCH=my-topic
+
+docker run -v `pwd`/../:/opt/bitnami/kafka/conf -it \
+ ${DOCKER_IMAGE} sh -c "kafka-topics.sh --create --bootstrap-server=${KAFKA_HOST}:${KAFKA_PORT} --topic ${KAFKA_BENCH} --replication-factor 3 --partitions 13 --command-config /opt/bitnami/kafka/conf/kafka-client/client.properties"
