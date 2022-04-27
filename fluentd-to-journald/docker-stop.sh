f7cb527cc216faf6a66569393aae80fd8e08775c
Jonas Cheng <jonas_cheng@trendmicro.com>
2022-04-27 14:21:19 +0800

    refactor: script refinement

--- fluentd-to-journald/docker-stop.sh
+++ fluentd-to-journald/docker-stop.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 docker-compose -f docker-compose.yml stop
-docker-compose -f fluentd/docker-compose.yml stop
--- fluentd-to-journald/docker-up.sh
+++ fluentd-to-journald/docker-up.sh
@@ -3,8 +3,4 @@
 docker rm -f `docker ps -qa`
 rm -rf fluentd/data
 
-docker-compose -f fluentd/docker-compose.yml up $@
-
-./wait-for-it.sh 127.0.0.1:24224 -t 60
-
 docker-compose -f docker-compose.yml up $@
