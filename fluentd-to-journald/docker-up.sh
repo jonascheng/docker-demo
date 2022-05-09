e00765ed242edd83f0d074117fb0f1160289066c
Jonas Cheng <jonas.cheng@gmail.com>
2022-05-09 15:30:36 +0800

    fix: fixed rotation issue

--- fluentd-to-journald/docker-up.sh
+++ fluentd-to-journald/docker-up.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 docker rm -f `docker ps -qa`
-rm -rf fluentd/data
-mkdir -p fluentd/data
-chmod a+w -R fluentd/data
+rm -rf /tmp/fluentd/data
+mkdir -p /tmp/fluentd/data
+#####
+# create empty log purposely
+touch /tmp/fluentd/data/info.log
+touch /tmp/fluentd/data/error.log
+touch /tmp/fluentd/data/fatal.log
+#####
+chmod a+w -R /tmp/fluentd/data
 
 docker-compose -f docker-compose.yml build
 docker-compose -f docker-compose.yml up $@
