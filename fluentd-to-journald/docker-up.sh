68a0df5b7795cd7a5bb1ed4534deda3578ec2079
Jonas Cheng <jonas_cheng@trendmicro.com>
2022-05-10 13:51:40 +0800

    fix: sudo command

--- fluentd-to-journald/docker-up.sh
+++ fluentd-to-journald/docker-up.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 docker rm -f `docker ps -qa`
-rm -rf /tmp/fluentd/data
-mkdir -p /tmp/fluentd/data
+sudo rm -rf /tmp/fluentd/data
+sudo mkdir -p /tmp/fluentd/data
 #####
 # create empty log purposely
-touch /tmp/fluentd/data/info.log
-touch /tmp/fluentd/data/error.log
-touch /tmp/fluentd/data/fatal.log
+sudo touch /tmp/fluentd/data/info.log
+sudo touch /tmp/fluentd/data/error.log
+sudo touch /tmp/fluentd/data/fatal.log
 #####
-chmod a+w -R /tmp/fluentd/data
+sudo chmod a+w -R /tmp/fluentd/data
 
 docker-compose -f docker-compose.yml build
 docker-compose -f docker-compose.yml up $@
