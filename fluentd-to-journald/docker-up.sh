d144f7989499d67c65278d8906e989efe1c4f1ab
Jonas Cheng <jonas.cheng@gmail.com>
2022-06-06 16:28:09 +0800

    update 5 files

--- fluentd-to-journald/docker-up.sh
+++ fluentd-to-journald/docker-up.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 docker rm -f `docker ps -qa`
-# sudo rm /var/log/journal/fluentd/*
+sudo rm /var/log/journal/fluentd/*
 # sudo mkdir -p /var/log/journal/fluentd/
 # sudo rm -rf /tmp/logrotate/data
 # sudo mkdir -p /tmp/logrotate/data
 
 docker-compose -f docker-compose.yml build
+
+sudo /usr/bin/timedatectl set-time 00:00:00
+
 docker-compose -f docker-compose.yml up $@
