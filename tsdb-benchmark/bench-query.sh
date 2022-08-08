2a46d6d7d4a95f721392975e76801634afd444f1
Jonas Cheng <jonas.cheng@gmail.com>
2022-08-08 15:57:41 +0800

    benchmark query

---
+++ tsdb-benchmark/bench-query.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+. ./getopts.sh >&2 >/dev/null
+
+echo === setup ===
+. ./setup.sh >&2 >/dev/null
+
+###
+echo === insert command ===
+docker exec -it tsdb sh -c "cat /tmp/insert-events-generate.sql"
+
+####
+echo
+echo "=== measure normal table query by last 7 days AND agent_id > 50 ==="
+docker exec -it tsdb psql -U postgres -f /sql/create-events-normaltable.sql >&2 >/dev/null
+docker exec -it tsdb psql -U postgres -f /tmp/insert-events-generate.sql >&2 >/dev/null
+time docker exec -it tsdb psql -U postgres -c "SELECT COUNT(1) FROM sample_events WHERE agent_id > 50 AND event_time > now() - INTERVAL '7 days';"
+echo === measure normal table query by last 1 days AND group by hour ===
+time docker exec -it tsdb psql -U postgres -c "SELECT date_trunc('hour', event_time) as hour, COUNT(1) FROM sample_events WHERE event_time > now() - INTERVAL '1 days' GROUP BY hour ORDER BY hour LIMIT 12;"
+
+####
+echo "=== measure hypertable query by last 7 days AND agent_id > 50 ==="
+docker exec -it tsdb psql -U postgres -f /tmp/create-events-hypertable.sql >&2 >/dev/null
+docker exec -it tsdb psql -U postgres -f /tmp/insert-events-generate.sql >&2 >/dev/null
+time docker exec -it tsdb psql -U postgres -c "SELECT COUNT(1) FROM sample_events WHERE agent_id > 50 AND event_time > now() - INTERVAL '7 days';"
+echo === measure normal table query by last 1 days AND group by hour ===
+time docker exec -it tsdb psql -U postgres -c "SELECT date_trunc('hour', event_time) as hour, COUNT(1) FROM sample_events WHERE event_time > now() - INTERVAL '1 days' GROUP BY hour ORDER BY hour LIMIT 12;"
+
+####
+
+####
+. ./teardown.sh
