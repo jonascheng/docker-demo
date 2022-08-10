6cff1d6230eb7ac9b19f4d9c8b00471f88d07819
Jonas Cheng <jonas_cheng@trendmicro.com>
2022-08-10 16:31:43 +0800

    feat: query with time_bucket

--- tsdb-benchmark/bench-query-group-by.sh
+++ tsdb-benchmark/bench-query-group-by.sh
@@ -19,7 +19,7 @@ docker exec -it tsdb psql -U postgres -f /tmp/insert-events-generate.sql >&2 >/d
 time docker exec -it tsdb psql -U postgres -c "SELECT date_trunc('hour', event_time) as hour, COUNT(1) FROM sample_events WHERE event_time > now() - INTERVAL '1 days' GROUP BY hour ORDER BY hour LIMIT 12;"
 
 ####
-echo === measure normal table query by last 1 days AND group by hour ===
+echo === measure hypertable query by last 1 days AND group by hour ===
 docker exec -it tsdb psql -U postgres -f /tmp/create-events-hypertable.sql >&2 >/dev/null
 docker exec -it tsdb psql -U postgres -f /tmp/insert-events-generate.sql >&2 >/dev/null
 time docker exec -it tsdb psql -U postgres -c "SELECT date_trunc('hour', event_time) as hour, COUNT(1) FROM sample_events WHERE event_time > now() - INTERVAL '1 days' GROUP BY hour ORDER BY hour LIMIT 12;"
