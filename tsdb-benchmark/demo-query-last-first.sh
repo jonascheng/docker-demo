945a8b7b273e082a2358c03c54bb6d731dd6dae6
Jonas Cheng <jonas_cheng@trendmicro.com>
2022-08-11 19:26:05 +0800

    feat: refined as name

--- tsdb-benchmark/demo-query-last-first.sh
+++ tsdb-benchmark/demo-query-last-first.sh
@@ -9,7 +9,7 @@ echo === setup ===
 echo === query by last 1 days AND group by 2 hours with time_bucket ===
 docker exec -it tsdb psql -U postgres -f /tmp/create-events-hypertable.sql >&2 >/dev/null
 docker exec -it tsdb psql -U postgres -f /tmp/insert-events-generate.sql >&2 >/dev/null
-docker exec -it tsdb psql -U postgres -c "SELECT time_bucket('2 hour', event_time) as two_hours, first(event_type, event_time), last(event_type, event_time), COUNT(1) FROM sample_events WHERE event_time > now() - INTERVAL '1 days' GROUP BY two_hours ORDER BY two_hours LIMIT 12;"
+docker exec -it tsdb psql -U postgres -c "SELECT time_bucket('2 hour', event_time) as two_hours, first(event_type, event_time) as first_event_type, last(event_type, event_time) as last_event_type, COUNT(1) FROM sample_events WHERE event_time > now() - INTERVAL '1 days' GROUP BY two_hours ORDER BY two_hours LIMIT 12;"
 
 ####
 . ./teardown.sh
